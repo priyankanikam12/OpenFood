b5a1d28ed9f80066edbf72777925a8fd4d1c8872
Priyanka Suryakant Nikam <127423033+priyankanikam12@users.noreply.github.com>
2024-10-22 12:59:04 +0530

    Base add in project

---
+++ OpenFood/OpenFood/Models/BuyVegetableViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenFood.Models
+{
+    public class BuyVegetableViewModel
+    {
+        public Vegetable Vegetable { get; set; }
+        public List<BuyingOption> BuyingOptions { get; set; }
+    }
+}
---
+++ OpenFood/OpenFood/Models/BuyingOption.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenFood.Models
+{
+    public class BuyingOption
+    {
+        public string PlatformName { get; set; }
+        public string Url { get; set; }
+    }
+}
